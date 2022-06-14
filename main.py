4ec31201eb78824a21f97dea3803abf5d7e5f710
Filip Helbig <filippiotrhelbig@gmail.com>
2022-06-14 19:44:14 +0200

    Added wait for load.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 from selenium import webdriver
-# from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Firefox()
 driver.get("https://avid-recruitment.azurewebsites.net")
@@ -15,6 +16,15 @@ sign_in_button = driver.find_element(By.ID, "sign-in-button")
 formUsername.send_keys("admin")
 formPassword.send_keys("testPassword")
 sign_in_button.click()
+
+wait = WebDriverWait(driver, 10)
+elem = wait.until(EC.presence_of_all_elements_located((By.ID, "formLimit")))
+
+formLimit = driver.find_element(By.ID, "formLimit")
+formLimit.send_keys("1")
+
+formQuery = driver.find_element(By.ID, "formQuery")
+formQuery.send_keys("copyTest")
 
 
 # driver.close()
